af8a97609aa388a1a5bafe18de3c782209aaeaa0
musa ngcobo <ngcobomusa950@gmail.com>
2019-10-07 12:55:32 +0200

    Updates

---
+++ precache-manifest.c1c02ad2dd8e19b58a32c3c77bd78c71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aac27e6fc7cc8f535c0963f12573c82",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7f19b0c3cff7514ac42d",
+    "url": "/profile/static/css/2.35d8b825.chunk.css"
+  },
+  {
+    "revision": "d420a961c0435606f0e1",
+    "url": "/profile/static/css/main.b5430b28.chunk.css"
+  },
+  {
+    "revision": "7f19b0c3cff7514ac42d",
+    "url": "/profile/static/js/2.e9cb93e9.chunk.js"
+  },
+  {
+    "revision": "d420a961c0435606f0e1",
+    "url": "/profile/static/js/main.2a20a2fb.chunk.js"
+  },
+  {
+    "revision": "63e6b6a09bc63e5f759e",
+    "url": "/profile/static/js/runtime~main.90728b17.js"
+  },
+  {
+    "revision": "9b850d0707723b6ca3901ca8099a5ffa",
+    "url": "/profile/static/media/Website.9b850d07.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/profile/static/media/react.47ce6e77.png"
+  }
+]);
